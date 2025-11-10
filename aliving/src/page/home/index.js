c53b52dc75fb8f2eff0d163aa69bcafcf90a1ead
20211896 <20211896@edu.hanbat.ac.kr>
2025-11-10 09:14:08 +0900

    feat : 메인 홈 생성

--- aliving/src/page/home/index.js
+++ aliving/src/page/home/index.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// styled-components를 사용하여 스타일 컴포넌트를 정의합니다.
-const HomeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh; /* 화면 전체 높이를 차지하도록 설정 */
-  text-align: center;
-`;
-
-const Title = styled.h1`
-  font-size: 3rem;
-  color: var(--primary-color); /* GlobalStyle에 정의된 CSS 변수 사용 */
-`;
-
-const Subtitle = styled.p`
-  font-size: 1.2rem;
-  color: var(--secondary-text-color); /* GlobalStyle에 정의된 CSS 변수 사용 */
-`;
 
 const HomePage = () => {
   return (
     <HomeContainer>
-      <Title>Aliving</Title>
-      <Subtitle>메인 홈 페이지에 오신 것을 환영합니다.</Subtitle>
+      <Title>반갑습니다!</Title>
+      <Title>유성구청 자치 프로그램 센터입니다</Title>
+      <Subtitle>유성구에서는 지역주민의 삶의 질 향상과 근거리학습권 보장을 위해, </Subtitle>
+      <Subtitle>동행정복지센터에서 각종 유익한 프로그램을 운영하고 있습니다. </Subtitle>
+      <Subtitle>아래의 버튼을 클릭하여 동 별행정복지센터 주민자치프로그램을 확인하실 수 있으니, 많은 참여 부탁드립니다.</Subtitle>
     </HomeContainer>
   );
 };
 
 export default HomePage;
+
+const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  color: var(--primary-color);
+`;
+
+const Subtitle = styled.p`
+  font-size: 1.2rem;
+  color: var(--secondary-text-color);
+`;
