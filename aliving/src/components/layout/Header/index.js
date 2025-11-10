0a39bfb4ff412021aa3dbc6504c02143d4d5cfeb
20211896 <20211896@edu.hanbat.ac.kr>
2025-11-10 09:15:24 +0900

    feat : 로그인 헤더 생성

---
+++ aliving/src/components/layout/Header/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as LogoSvg } from '../../../assets/logo.svg';
+
+
+const Header = () => {
+    return (
+        <HeaderContainer>
+            <Logo href="/"><LogoSvg /></Logo>
+
+            <NavLinks>
+                <Button>로그인</Button>
+                <Button>회원가입</Button>
+            </NavLinks>
+        </HeaderContainer>
+    );
+};
+
+export default Header;
+
+
+
+const HeaderContainer = styled.header`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  padding: 1rem 2rem;
+  background-color: #ffffff;
+  border-bottom: 1px solid #eaeaea;
+`;
+
+const Logo = styled.a`
+  text-decoration: none;
+  
+  svg {
+    height: 2.5rem;
+    width: auto;
+  }
+`;
+
+const Text = styled.span`
+  font-size: 1rem;
+  color: #555;
+  margin-left: 1rem;
+`;
+
+const NavLinks = styled.nav`
+  display: flex;
+  gap: 1rem;
+  margin-left: auto;
+`;
+
+const Button = styled.button`
+  width: auto;
+  height: 2.5rem;
+  padding: 0.5rem 1rem;
+  background-color: #0070f3;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  font-size: 1rem;
+  font-weight: bold;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    background-color: #005bb5;
+  }
+`;
