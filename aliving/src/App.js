c34522865523b2e3b468bb7c959fdabdfd15a947
dmd-sik <apkool12@naver.com>
2025-12-12 11:43:54 +0900

    [Feat] - Update routing with new pages and add Kakao button icon

--- aliving/src/App.js
+++ aliving/src/App.js
@@ -1,35 +1,79 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import styled from "styled-components";
 import GlobalStyle from "./GlobalStyle";
+import LoginPage from "./page/login";
+import SignupPage from "./page/signup";
 import HomePage from "./page/home";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import UserDongPage from "./page/userDong";
 import ProgramDetail from "./page/userDong/ProgramDetail";
-import AdminDongPage from './page/adminDong';
-import ProgramCreatePage from './page/adminDong/programCreate';
-import ProgramEditPage from './page/adminDong/programEdit';
-import ProgramSuccessPage from './page/adminDong/programSuccess';
+import ProgramApplication from "./page/userDong/ProgramApplication";
+import ProgramApplicationSuccess from "./page/userDong/ProgramApplicationSuccess";
+import AdminDongPage from "./page/adminDong";
+import ProgramCreatePage from "./page/adminDong/programCreate";
+import ProgramEditPage from "./page/adminDong/programEdit";
+import ProgramSuccessPage from "./page/adminDong/programSuccess";
+
+function AppContent() {
+  const location = useLocation();
+  const hideHeaderFooter =
+    location.pathname.includes("/apply") &&
+    !location.pathname.includes("/success");
+
+  return (
+    <>
+      {!hideHeaderFooter && <Header />}
+      <Main>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/dong/:dongName" element={<UserDongPage />} />
+          <Route
+            path="/dong/:dongName/program/:programId"
+            element={<ProgramDetail />}
+          />
+          <Route
+            path="/dong/:dongName/program/:programId/apply"
+            element={<ProgramApplication />}
+          />
+          <Route
+            path="/dong/:dongName/program/:programId/apply/success"
+            element={<ProgramApplicationSuccess />}
+          />
+          <Route path="/admin/dong/:dongName" element={<AdminDongPage />} />
+          <Route
+            path="/admin/dong/:dongName/add"
+            element={<ProgramCreatePage />}
+          />
+          <Route
+            path="/admin/dong/:dongName/edit/:programId"
+            element={<ProgramEditPage />}
+          />
+          <Route
+            path="/admin/dong/:dongName/success"
+            element={<ProgramSuccessPage />}
+          />
+        </Routes>
+      </Main>
+      {!hideHeaderFooter && <Footer />}
+    </>
+  );
+}
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Router>
-        <Header />
-        <Main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/dong/:dongName" element={<UserDongPage />} />
-            <Route path="/dong/:dongName/program/:programId" element={<ProgramDetail />} />
-            <Route path="/admin/dong/:dongName" element={<AdminDongPage />} />
-            <Route path="/admin/dong/:dongName/add" element={<ProgramCreatePage />} />
-            <Route path="/admin/dong/:dongName/edit/:programId" element={<ProgramEditPage />} />
-            <Route path="/admin/dong/:dongName/success" element={<ProgramSuccessPage />} />
-          </Routes>
-        </Main>
-        <Footer />
+        <AppContent />
       </Router>
     </>
   );
@@ -38,7 +82,7 @@ function App() {
 export default App;
 
 const Main = styled.main`
-  flex: 1; 
+  flex: 1;
   display: flex;
   flex-direction: column;
 `;
